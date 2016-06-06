449342e9548cfec02fd093f5a3ded835f3bfd3d8
zagt <zagt@ZAGTdeiMac.local>
2016-06-06 18:28:02 +0800

    commit
    commit change

---
+++ zagtApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by ZAGT on 16/6/2.
+//  Copyright © 2016年 ZAGT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
